8bf13bf0db0196a75be88ab937a899baea17169d
robin paul <cachebroker@gmail.com>
2016-02-17 12:02:07 -0600

    pulsar dashboard

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+	root 'dashboard#index'
+	resources :customers, only: [:index, :show, :update]
+
+	get 'angular_test', to: "angular_test#index"
+	get "fake_billing", to: "fake_billing#show"
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,55 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/poltergeist'
+
+Capybara.javascript_driver = :poltergeist
+Capybara.default_driver = :poltergeist
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = false
+
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+
+  # config.add_setting :screenshots_dir
+  # config.screenshots_dir = "#{::Rails.root}/spec/screenshots"
+  # config.include(EasyScreenshots, type: :feature)
+  # config.include(FactoryGirl::Syntax::Methods)
+  # config.include(Devise::TestHelpers, type: :controller)
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type:   :feature) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
